7341f0c6df99e443625a8f37f52d060d87d0df57
eduprog <eduprog@gmail.com>
2024-09-09 15:56:37 -0300

    Atualizar para .NET 8, adicionar Serilog e melhorar a segurança
    Atualizar a estrutura de destino para .NET 8.0 para vários projetos, incluindo BaGetWebApplication, BaGet.Aliyun e outros. Adicionar novas referências de pacote em BaGet.csproj: BouncyCastle.NetCore, Serilog.AspNetCore, Serilog.Sinks.Console e Serilog.Sinks.File.
    
    Aprimorar o registro em Program.cs e Startup.cs usando Serilog, configurar o Kestrel para carregar certificados da configuração e adicionar um método para carregar certificados de arquivos .crt e .key.
    
    Modificar o tratamento de exceções em TablePackageDatabase.cs removendo filtros de exceção específicos. Alterar URL do aplicativo em launchSettings.json para usar HTTPS. Adicionar seções de configuração de certificado em appsettings.json e appsettings.Development.json.
    
    Pequenos ajustes de formatação e espaço em branco em vários arquivos.
    
    Colocado a informação de certificado e senh ano appsetings.json

--- src/BaGet/Program.cs
+++ src/BaGet/Program.cs
@@ -1,6 +1,8 @@
 using System;
 using System.IO;
 using System.Net;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using BaGet.Core;
 using BaGet.Web;
@@ -10,7 +12,18 @@ using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Operators;
+using Org.BouncyCastle.OpenSsl;
+using Org.BouncyCastle.X509;
+using Org.BouncyCastle.Asn1.X509;
+using Org.BouncyCastle.Asn1;
+using System.Security.Cryptography;
+using Microsoft.Extensions.Logging;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Security;
+using Serilog;
+using X509Certificate = Org.BouncyCastle.X509.X509Certificate;
 
 /* 
     - Publicando o projeto Nuget Server 
@@ -49,6 +62,7 @@ namespace BaGet
 {
     public class Program
     {
+
         public static async Task Main(string[] args)
         {
             var host = CreateHostBuilder(args).Build();
@@ -94,8 +108,21 @@ namespace BaGet
 
         public static IHostBuilder CreateHostBuilder(string[] args)
         {
+            var logFilePath = Path.Combine(AppContext.BaseDirectory, "logs", "log.txt");
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Console()  // Write logs to the console
+                .WriteTo.File(logFilePath, rollingInterval: RollingInterval.Day, retainedFileCountLimit: 30)  // Write logs to a file
+                .CreateLogger();
+
+
             var host = Host
                 .CreateDefaultBuilder(args)
+                //.ConfigureServices((context, services) =>
+                //{
+                //    services.AddLogging();
+                //})
+                .UseSerilog()
                 .ConfigureAppConfiguration((ctx, config) =>
                 {
                     var root = Environment.GetEnvironmentVariable("BAGET_CONFIG_ROOT");
@@ -105,7 +132,13 @@ namespace BaGet
                         config.SetBasePath(root);
                     }
                 })
-                .ConfigureWebHostDefaults(web =>
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();  // Clear other logging providers
+                    // Serilog is already set up, so no need to add Console or File here
+                    logging.SetMinimumLevel(LogLevel.Debug); // Ajuste conforme necessário
+                })
+                .ConfigureWebHostDefaults((web) =>
                 {
                     //web.UseUrls(
                     //    //$"http://*:61437",
@@ -113,7 +146,7 @@ namespace BaGet
                     web
                         //.UseUrls()
                         //.UseKestrel()
-                        .ConfigureKestrel(options =>
+                        .ConfigureKestrel((context, options) =>
                     {
                         var port = 61438;
                         // Remove the upload limit from Kestrel. If needed, an upload limit can
@@ -122,21 +155,105 @@ namespace BaGet
                         //options.ListenAnyIP(61437);
                         options.Listen(IPAddress.Any, port, listenOptions =>
                         {
-                            var certPath = Path.Combine(AppContext.BaseDirectory, "Certificados", "certificate.pfx");
+                            var config = context.Configuration;
+
+                            var certPath = config["Certificado:PathFileCrt"];
+                            var keyPath = config["Certificado:PathFileKey"];
+                            var certPfx = config["Certificado:PathFilePfx"];
+                            var password = config["Certificado:Senha"];
+
+
+
+
+                            //var certPath = Path.Combine(AppContext.BaseDirectory, "Certificados", "nuget.esistem.com.br.pfx");
+                            //var certPath = config.Configuration["Certificado:PathFileCrt"];
                             //////var keyPath = Path.Combine(AppContext.BaseDirectory, "Certificados", "key.pem");
-                            var password = "#eSistem2023@";
+                            //var password = config.Configuration["Certificado:Senha"];//"#eSistem2023@";
                             //var cert = new X509Certificate2(certPath);
                             //var connectionOptions = new HttpsConnectionAdapterOptions();
                             //connectionOptions.ServerCertificate = cert;
-                            listenOptions.Protocols = HttpProtocols.Http1AndHttp2;
-                            listenOptions.UseHttps(certPath,password);
-                        });
-                    });
 
-                    web.UseStartup<Startup>();
+                            if (!string.IsNullOrEmpty(certPath) && !string.IsNullOrEmpty(password) && !string.IsNullOrEmpty(keyPath))
+                            {
+                                var certificate = LoadCertificate(certPath, keyPath, password);
+                                listenOptions.Protocols = HttpProtocols.Http1AndHttp2;
+                                listenOptions.UseHttps(certificate);
+                            }
+                            else if (!string.IsNullOrEmpty(certPfx) && !string.IsNullOrEmpty(password))
+                            {
+                                listenOptions.Protocols = HttpProtocols.Http1AndHttp2;
+                                listenOptions.UseHttps(certPfx, password);
+                            }
+                            else
+                            {
+                                throw new Exception("Certificado não configurado corretamente.");
+                            }
+                        });
+                    }).UseStartup<Startup>();
+
                 });
 
             return host;
+        }
+
+
+        public static X509Certificate2 LoadCertificate(string certPath, string keyPath, string password)
+        {
+            // Leitura do certificado .crt
+            var certPem = File.ReadAllText(certPath);
+
+            // Leitura da chave .key
+            var keyPem = File.ReadAllText(keyPath);
+
+            // Carregar o certificado
+            var certParser = new PemReader(new StringReader(certPem));
+            var cert = (X509Certificate)certParser.ReadObject();
+
+            // Carregar a chave privada
+            var keyParser = new PemReader(new StringReader(keyPem));
+
+            var keyObject = keyParser.ReadObject();
+            RsaPrivateCrtKeyParameters privateKey;
+
+            switch (keyObject)
+            {
+                case AsymmetricCipherKeyPair keyPair:
+                    privateKey = keyPair.Private as RsaPrivateCrtKeyParameters;
+                    break;
+                case RsaPrivateCrtKeyParameters rsaPrivateKey:
+                    privateKey = rsaPrivateKey;
+                    break;
+                default:
+                    throw new InvalidCastException("Unsupported key type.");
+            }
+
+
+            //var keyPair = (AsymmetricCipherKeyPair)keyParser.ReadObject();
+            //var privateKey = keyPair.Private as RsaPrivateCrtKeyParameters;
+
+            // Converter chave para o formato que o X509Certificate2 pode aceitar
+            var rsa = RSA.Create();
+            if (privateKey != null)
+                rsa.ImportParameters(new RSAParameters
+                {
+                    Modulus = privateKey.Modulus.ToByteArrayUnsigned(),
+                    Exponent = privateKey.PublicExponent.ToByteArrayUnsigned(),
+                    P = privateKey.P.ToByteArrayUnsigned(),
+                    Q = privateKey.Q.ToByteArrayUnsigned(),
+                    DP = privateKey.DP.ToByteArrayUnsigned(),
+                    DQ = privateKey.DQ.ToByteArrayUnsigned(),
+                    InverseQ = privateKey.QInv.ToByteArrayUnsigned(),
+                    D = privateKey.Exponent.ToByteArrayUnsigned()
+                });
+
+            // Criar o certificado X509
+            var certBytes = cert.GetEncoded();
+            var certificate = new X509Certificate2(certBytes);
+
+            // Adicionar a chave privada ao certificado
+            var certWithKey = certificate.CopyWithPrivateKey(rsa);
+
+            return certWithKey;
         }
     }
 }
